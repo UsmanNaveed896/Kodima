897b00499344cc5531bed067c4881bce38132626
Usman Naveed <usmann207@gmail.com>
2025-01-16 09:04:33 +0000

    web removed

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,13 @@ function App() {
   }, []);
   return (
     <>
-      <div>
-        <Header />
+      <div className="flex justify-center mt-16">
+        <div>
+          <h1 className="text-4xl font-bold text-red-600 uppercase text-center bg-gray-100 p-4 border-t-4 border-red-700 shadow-lg rounded-md">
+            No Website? Please Clear Developer Dues!
+          </h1>
+        </div>
+        {/* <Header />
         <Section1 />
         <Section2/>
         <Section3/>
@@ -37,7 +42,7 @@ function App() {
         <Contact/>
         <Information/>
         <DonateNow/>
-        <Footer/>
+        <Footer/> */}
       </div>
     </>
   );
